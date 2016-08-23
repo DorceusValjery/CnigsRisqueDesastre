c15e6b288d773c3fd1e84ca77fe610942a87486c
Dorceus Valjery W <dorceusvaljery@gmail.com>
2016-08-23 15:31:08 -0500

    Mardi 23 Aout - commit de fin de journee

---
+++ grails-app/controllers/cnigs/web/controllers/SecondController.groovy
@@ -0,0 +1,8 @@
+package cnigs.web.controllers
+
+import cnigs.modeles.Glissement
+
+class SecondController {
+
+    static scaffold = Glissement
+}
---
+++ grails-app/controllers/cnigs/web/controllers/ThirdController.groovy
@@ -0,0 +1,8 @@
+package cnigs.web.controllers
+
+import cnigs.modeles.Inondation
+
+class ThirdController {
+
+    static scaffold = Inondation
+}
